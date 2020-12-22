2739e71a78c43483b956af61d48d983ff2d451cb
Melanie Safira <melaniesafira94@gmail.com>
2020-12-22 12:33:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/blogku/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.blogku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageButton btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnBack = findViewById(R.id.btn_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
